d7cfe6d604d01bbadf772413257b933824ba8f7e
AFaustini <32999190+AFaustini@users.noreply.github.com>
2019-11-13 14:54:55 -0300

    Correção na remoção dos Apps da Store

--- Otimizacao.bat
+++ Otimizacao.bat
@@ -559,7 +559,7 @@ DISM.exe /Online /norestart /Disable-Feature /featurename:FaxServicesClientPacka
 DISM.exe /Online /norestart /Disable-Feature /featurename:Printing-Foundation-InternetPrinting-Client /Remove
 
 REM Remover Apps da Store
-Get-AppxPackage | where-object {$_.name –notlike '*store*'} | Remove-AppxPackage
+Powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Get-AppxPackage | where-object {$_.name -notlike '*store*'} | Remove-AppxPackage"
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "FeatureManagementEnabled" /t REG_DWORD /d 0 /f
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "OemPreInstalledAppsEnabled" /t REG_DWORD /d 0 /f
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "PreInstalledAppsEnabled" /t REG_DWORD /d 0 /f
