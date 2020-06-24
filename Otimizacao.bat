18bf03f9acdd1a795d4bc5db722015a458aa860e
AFaustini <32999190+AFaustini@users.noreply.github.com>
2020-06-24 17:12:59 -0300

    Habiltar agendamento de aceleração de GPU

--- Otimizacao.bat
+++ Otimizacao.bat
@@ -644,7 +644,7 @@ REM *** Desabilitar Assistência Remota ***
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Remote Assistance" /v fAllowToGetHelp /d 0 /t REG_DWORD /f
 
 REM *** Habiltar agendamento de aceleração de GPU ***
-REM REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v HwSchMode /d 2 /t REG_DWORD /f
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v HwSchMode /d 2 /t REG_DWORD /f
 
 REM ***Instalar Clientes de Jogos ***
 REM choco install epicgameslauncher -y
