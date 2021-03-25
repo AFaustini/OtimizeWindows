17245d7518882327105a89afa666cd04cd1ab099
AFaustini <32999190+AFaustini@users.noreply.github.com>
2021-03-25 16:28:57 -0300

    Servicos modificados a cada build insider

---
+++ Otimizacao_Insider.bat
@@ -0,0 +1,164 @@
+@rem *** Desabilitar alguns serviços ***
+sc stop DiagTrack
+sc stop diagnosticshub.standardcollector.service
+sc stop dmwappushservice
+sc stop RemoteRegistry
+sc stop TrkWks
+sc stop WMPNetworkSvc
+sc stop SysMain
+sc stop lmhosts
+sc stop VSS
+sc stop RemoteAccess
+sc stop WSearch
+sc stop iphlpsvc
+sc stop DoSvc
+sc stop ICEsoundService
+sc stop ClickToRunSvc
+sc stop SEMgrSvc
+sc stop RtkAudioUniversalService
+sc stop BDESVC
+sc stop TabletInputService
+sc stop SstpSvc
+sc stop NvTelemetryContainer
+sc stop HomeGroupListener
+sc stop HomeGroupProvider
+sc stop lfsvc
+sc stop MapsBroke
+sc stop NetTcpPortSharing
+sc stop SharedAccess
+sc stop WbioSrvc
+sc stop WMPNetworkSvc
+sc stop wisvc
+sc stop TapiSrv
+sc stop SmsRouter
+sc stop SharedRealitySvc
+sc stop ScDeviceEnum
+sc stop SCardSvr
+sc stop RetailDemo
+sc stop PhoneSvc
+sc stop perceptionsimulation
+sc stop BTAGService
+sc stop AJRouter
+sc stop CDPSvc
+sc stop ShellHWDetection
+sc stop RstMwService
+sc stop DusmSvc
+sc stop BthAvctpSvc
+sc stop BITS
+sc stop DPS
+
+sc config DiagTrack start= disabled
+sc config diagnosticshub.standardcollector.service start= disabled
+sc config dmwappushservice start= disabled
+sc config RemoteRegistry start= disabled
+sc config TrkWks start= disabled
+sc config WMPNetworkSvc start= disabled
+sc config SysMain start= disabled
+sc config lmhosts start= disabled
+sc config VSS start= disabled
+sc config RemoteAccess start= disabled
+sc config WSearch start= disabled
+sc config iphlpsvc start= disabled
+sc config DoSvc start= disabled
+sc config ICEsoundService start= disabled
+sc config ClickToRunSvc start= demand
+sc config SEMgrSvc start= disabled
+sc config RtkAudioUniversalService start= disabled
+sc config BDESVC start= disabled
+sc config TabletInputService start= disabled
+sc config SstpSvc start= disabled
+sc config NvTelemetryContainer start= disabled
+sc config HomeGroupListener start= disabled
+sc config HomeGroupProvider start= disabled
+sc config lfsvc start= disabled
+sc config MapsBroke start= disabled
+sc config NetTcpPortSharing start= disabled
+sc config SharedAccess start= disabled
+sc config WbioSrvc start= disabled
+sc config WMPNetworkSvc start= disabled
+sc config wisvc start= disabled
+sc config TapiSrv start= disabled
+sc config SmsRouter start= disabled
+sc config SharedRealitySvc start= disabled
+sc config ScDeviceEnum start= disabled
+sc config SCardSvr start= disabled
+sc config RetailDemo start= disabled
+sc config PhoneSvc start= disabled
+sc config perceptionsimulation start= disabled
+sc config BTAGService start= disabled
+sc config AJRouter start= disabled
+sc config CDPSvc start= disabled
+sc config ShellHWDetection start= disabled
+sc config RstMwService start= disabled
+sc config DusmSvc start= disabled
+sc config BthAvctpSvc start= disabled
+sc config BITS start= demand
+sc config DPS start= disabled
+
+REM *** Tweaks de tarefas agendadas ***
+schtasks /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /Disable
+schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable
+schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /Disable
+schtasks /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /Disable
+schtasks /Change /TN "Microsoft\Windows\Autochk\Proxy" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Uploader" /Disable
+schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable
+schtasks /Change /TN "Microsoft\Windows\Maintenance\WinSAT" /Disable
+schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyUpload" /Disable
+schtasks /Change /TN "Microsoft\Windows\SystemRestore\SR" /Disable
+schtasks /Change /TN "Microsoft\Office\Office Automatic Updates 2.0" /Disable
+schtasks /Change /TN "Microsoft\Office\Office ClickToRun Service Monitor" /Disable
+schtasks /Change /TN "Microsoft\Office\Office Feature Updates" /Disable
+schtasks /Change /TN "Microsoft\Office\Office Feature Updates Logon" /Disable
+schtasks /Change /TN "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /Disable
+schtasks /Change /TN "MicrosoftEdgeUpdateTaskMachineCore" /Disable
+schtasks /Change /TN "MicrosoftEdgeUpdateTaskMachineUA" /Disable
+schtasks /Change /TN "Microsoft\Windows\Autochk\Proxy" /Disable
+schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable
+schtasks /Change /TN "Microsoft\Windows\FileHistory\File History (maintenance mode)" /Disable
+schtasks /Change /TN "Microsoft\Windows\Maintenance\WinSAT" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\DiskFootprint\Diagnostics" /Disable *** Not sure if should be disabled, maybe related to S.M.A.R.T.
+REM schtasks /Change /TN "Microsoft\Windows\NetTrace\GatherNetworkInfo" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\PI\Sqm-Tasks" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\Time Synchronization\ForceSynchronizeTime" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\Time Synchronization\SynchronizeTime" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\Windows Error Reporting\QueueReporting" /Disable
+
+@REM Desabilitar Sensor de Wifi
+reg add "HKLM\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" /v value /t REG_DWORD /d 0 /f
+
+@REM Mudar updates para notificar o agendamento de reinicialização
+reg add "HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" /v UxOption /t REG_DWORD /d 1 /f
+
+REM *** Desabilitar Superfetch ***
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnablePrefetcher" /t REG_DWORD /d 00000000 /f
+
+REM *** Tweaks Variados ***
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v "MenuShowDelay" /t REG_SZ /d 5 /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v "WaitToKillAppTimeout" /t REG_SZ /d 3000 /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v "ActiveWndTrackTimeout" /t REG_DWORD /d 0000000a /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control" /v "WaitToKillServiceTimeout" /t REG_SZ /d 2000 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control" /v "WaitToKillServiceTimeout" /t REG_SZ /d 2000 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableFirstLogonAnimation" /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "EnableFirstLogonAnimation" /t REG_DWORD /d 0 /f
+
+REM *** Desabilitar download automatico de drivers ***
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d 0 /f
+
+REM *** Desabilitar Hotspot 2.0 ***
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\icssvc" /V Start /T REG_DWORD /D 4 /F
+
+REM *** Desabilitar Histórico do Acesso Rápido ***
+REG delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HomeFolderDesktop\NameSpace\DelegateFolders\{3936E9E4-D92C-4EEE-A85A-BC16D5EA0819}" /f
+REG delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\HomeFolderDesktop\NameSpace\DelegateFolders\{3936E9E4-D92C-4EEE-A85A-BC16D5EA0819}" /f
+REG delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HomeFolderDesktop\NameSpace\DelegateFolders\{3134ef9c-6b18-4996-ad04-ed5912e00eb5}" /f
+REG delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\HomeFolderDesktop\NameSpace\DelegateFolders\{3134ef9c-6b18-4996-ad04-ed5912e00eb5}" /f
+
+TIMEOUT /T 5
+taskkill /f /im explorer.exe
+start explorer.exe
+msg %username% Otimizacao Finalizada com Sucesso
